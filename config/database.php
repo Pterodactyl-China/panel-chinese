46371536586d6cbeac4b8d73ddf4ed9b24ca864e
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-09-10 00:31:12 +0800

    数据库 IP 设置 默认指定为 localhost

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'panel'),
             'username' => env('DB_USERNAME', 'pterodactyl'),
