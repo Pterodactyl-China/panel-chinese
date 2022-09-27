47c803090431a282c366d8683bcd15018e79e845
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2022-09-27 22:09:31 +0800

    挂载页提示汉化

--- app/Http/Controllers/Admin/ServersController.php
+++ app/Http/Controllers/Admin/ServersController.php
@@ -388,7 +388,7 @@ class ServersController extends Controller
 
         $mountServer->saveOrFail();
 
-        $this->alert->success('Mount was added successfully.')->flash();
+        $this->alert->success('已成功添加挂载。')->flash();
 
         return redirect()->route('admin.servers.view.mounts', $server->id);
     }
@@ -402,7 +402,7 @@ class ServersController extends Controller
     {
         MountServer::where('mount_id', $mount->id)->where('server_id', $server->id)->delete();
 
-        $this->alert->success('Mount was removed successfully.')->flash();
+        $this->alert->success('已成功移除挂载。')->flash();
 
         return redirect()->route('admin.servers.view.mounts', $server->id);
     }
