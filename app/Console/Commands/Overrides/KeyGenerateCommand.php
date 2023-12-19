8cc26c236636de3c369e4b14e96c16bf3415b63c
飒爽师叔 <43847794+vlssu@users.noreply.github.com>
2023-12-19 09:18:46 +0800

    Update KeyGenerateCommand.php

--- app/Console/Commands/Overrides/KeyGenerateCommand.php
+++ app/Console/Commands/Overrides/KeyGenerateCommand.php
@@ -13,12 +13,12 @@ class KeyGenerateCommand extends BaseKeyGenerateCommand
     public function handle()
     {
         if (!empty(config('app.key')) && $this->input->isInteractive()) {
-            $this->output->warning('It appears you have already configured an application encryption key. Continuing with this process with overwrite that key and cause data corruption for any existing encrypted data. DO NOT CONTINUE UNLESS YOU KNOW WHAT YOU ARE DOING.');
-            if (!$this->confirm('I understand the consequences of performing this command and accept all responsibility for the loss of encrypted data.')) {
+            $this->output->warning('您似乎已经配置了应用程序加密密钥。如果继续此过程将会覆盖该密钥并导致任何现有已加密数据的损坏。除非您知道自己在做什么，否则请勿继续。');
+            if (!$this->confirm('我了解执行此命令的后果，并承担对加密数据丢失的所有责任。')) {
                 return;
             }
 
-            if (!$this->confirm('Are you sure you wish to continue? Changing the application encryption key WILL CAUSE DATA LOSS.')) {
+            if (!$this->confirm('您确定要继续吗？更改应用程序加密密钥将将导致数据丢失。')) {
                 return;
             }
         }
